8727466da9db03b8bf5c907344a36503a4e69078
Sherry Cuenco <sjcuenco@rocketmail.com>
2020-02-04 23:45:00 -0500

    Added encryption, auth service, signup feature.

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,28 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
 const router = express.Router();
+const User = require('../models/user');
 
 router.post('/signup', (req, res, next) => {
-
+  bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+      const user = new User({
+        email: req.body.email,
+        password: hash
+      });
+      user.save()
+        .then(createdUser => {
+          res.status(201).json({
+            message: 'User added successfully',
+            result: createdUser
+          });
+        })
+        .catch(err => {
+          res.status(500).json({
+            error: err
+          });
+        });
+    });
 });
 
 module.exports = router;
